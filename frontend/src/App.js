e07b43fabecc997405ec644323ded84a9f24beb4
Henry Diamond <henrydiamond94@gmail.com>
2021-07-07 12:13:08 +0400

    React router dom integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
-import React from "react";
-import {Container}from "react-bootstrap"
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 const App = () => {
-  return (
-    <>
-    <Header/>
-    <main className="py-3">
-    <Container>
-    <HomeScreen/>
-    </Container>
-    </main>
-    <Footer/>
-    </>
-  );
-}
+	return (
+		<Router>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<Switch>
+						<Route exact path='/' component={HomeScreen} />
+						<Route path='/product/:id' component={ProductScreen} />
+					</Switch>
+				</Container>
+			</main>
+			<Footer />
+		</Router>
+	);
+};
 
 export default App;
