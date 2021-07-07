fabeb11b4792c5b834a776c0cd5a1ec648c6f164
Henry Diamond <henrydiamond94@gmail.com>
2021-07-07 14:31:52 +0400

    Fetching product from react useEffect

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import { Row,Col } from 'react-bootstrap'
-import Product from "../components/Product"
-import products from "../products"
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
 
 const HomeScreen = () => {
-    return (
-        <>
-          <h1>Latest Products</h1> 
-          <Row>
-                {products.map((product)=>(
-                    <Col sm={12} md={6} xl={3} key={product._id} >
-                        <Product product={product}/>
-                    </Col>
-                ))}
-          </Row> 
-        </>
-    )
-}
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const { data } = await axios.get('/api/products');
+			setProducts(data);
+		};
+		fetchProduct();
+	}, []);
+	return (
+		<>
+			<h1>Latest Products</h1>
+			<Row>
+				{products.map(product => (
+					<Col sm={12} md={6} xl={3} key={product._id}>
+						<Product product={product} />
+					</Col>
+				))}
+			</Row>
+		</>
+	);
+};
 
 export default HomeScreen;
