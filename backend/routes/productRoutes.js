ed931af64521604161c5a4270639d20e224e895a
Henry Diamond <henrydiamond94@gmail.com>
2021-07-08 23:25:24 +0400

    Custom error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -11,6 +11,8 @@ router.get(
 	asyncHandler(async (req, res) => {
 		const products = await Product.find({});
 		res.json(products);
+		// return products;
+		console.log(res.statusCode);
 	})
 );
 // @desc 	Fetch single product
@@ -20,8 +22,12 @@ router.get(
 	'/:id',
 	asyncHandler(async (req, res) => {
 		const product = await Product.findById(req.params.id);
-		if (!product) res.status(404).json({ message: 'Product not found' });
-		res.json(product);
+		if (product) {
+			res.json(product);
+		} else {
+			res.status(404);
+			throw new Error('Product not found');
+		}
 	})
 );
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDb from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 // Routes
 import productRoutes from './routes/productRoutes.js';
@@ -10,6 +11,8 @@ dotenv.config();
 connectDb();
 const app = express();
 app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(
