332794bc6579e5b55ca0dcee85f69a7a36c58ca3
Henry Diamond <henrydiamond94@gmail.com>
2021-07-09 19:22:20 +0400

    Qty Select & Add To Cart Button

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { listProductDetails } from '../actions/productActions';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import {
+	Row,
+	Col,
+	Image,
+	ListGroup,
+	Card,
+	Button,
+	Form,
+} from 'react-bootstrap';
 import Rating from '../components/Rating';
 import Spinner from '../components/Spinner';
 import Message from '../components/Message';
 
 const ProductScreen = ({ match }) => {
+	const [qty, setQty] = useState(0);
 	const dispatch = useDispatch();
 	const productDetails = useSelector(state => state.productDetails);
 	const { loading, product, error } = productDetails;
@@ -66,6 +75,24 @@ const ProductScreen = ({ match }) => {
 										</Col>
 									</Row>
 								</ListGroup.Item>
+								{product.countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col>Qty</Col>
+											<Col>
+												<Form.Control
+													as='select'
+													value={qty}
+													onChange={e => setQty(e.target.value)}
+												>
+													{[...Array(product.countInStock).keys()].map(x => (
+														<option key={x + 1}>{x + 1}</option>
+													))}
+												</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
 								<ListGroup.Item>
 									<Button
 										className='btn-block'
