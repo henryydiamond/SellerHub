7aee186cf555add06b41c9ec44cfb62d6dd6c638
Henry Diamond <henrydiamond94@gmail.com>
2021-07-10 20:06:00 +0400

    Cart Reducer & Add To Cart Action

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -4,8 +4,8 @@ import { Alert } from 'react-bootstrap';
 const Message = ({ variant, children }) => {
 	return (
 		<Alert variant={variant}>
-			<i className='fas fa-exclamation-circle' />
-			{children}
+			<i className='fas fa-exclamation-circle fa-lg' />
+			<span style={{ fontSize: '15px', marginLeft: '10px' }}> {children}!</span>
 		</Alert>
 	);
 };
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Message from '../components/Message';
 
-const CartScreen = () => {
-	return <div>Cart</div>;
+const CartScreen = ({ match }) => {
+	if (!match.params.id) {
+		return <Message variant='info' children='You have no item in your cart' />;
+	} else {
+		return <h1>Cart</h1>;
+	}
 };
 
 export default CartScreen;
