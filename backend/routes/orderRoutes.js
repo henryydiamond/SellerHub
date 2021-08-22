ffc9afc673f6dbec9e0af751aadb5ed4e8b52dde
Henry Diamond <henrydiamond94@gmail.com>
2021-08-22 14:44:25 +0400

    Update To Paid Endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,9 +2,14 @@ import express from 'express';
 const router = express.Router();
 
 import protect from '../middleware/authMiddleware.js';
-import { addOrderItems, getOrderById } from '../controlers/orderControler.js';
+import {
+	addOrderItems,
+	getOrderById,
+	updateOrderToPaid,
+} from '../controlers/orderControler.js';
 
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
