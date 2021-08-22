54f1b3578f9dc5d275ff10ee6bf93641e3a8e236
Henry Diamond <henrydiamond94@gmail.com>
2021-08-22 04:31:37 +0400

    Order Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 const App = () => {
 	return (
 		<Router>
@@ -19,6 +20,7 @@ const App = () => {
 			<main className='py-3'>
 				<Container>
 					<Switch>
+						<Route path='/order/:id' component={OrderScreen} />
 						<Route path='/placeorder' component={PlaceOrderScreen} />
 						<Route path='/shipping' component={ShippingScreen} />
 						<Route path='/payment' component={PaymentScreen} />
