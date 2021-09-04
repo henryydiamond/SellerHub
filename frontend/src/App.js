0df9a8792b1ab911130424bd93298b05797736f6
Henry Diamond <henrydiamond94@gmail.com>
2021-09-05 02:56:43 +0400

    Edit Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 const App = () => {
 	return (
 		<Router>
@@ -23,9 +24,14 @@ const App = () => {
 			<main className='py-3'>
 				<Container>
 					<Switch>
-						<Route path='/admin/userlist' component={UserListScreen} />
 						<Route path='/admin/productlist' component={ProductListScreen} />
+						<Route path='/product/:id' component={ProductScreen} />
+						<Route
+							path='/admin/product/:id/edit'
+							component={ProductEditScreen}
+						/>
 						<Route path='/admin/user/:id/edit' component={UserEditScreen} />
+						<Route path='/admin/userlist' component={UserListScreen} />
 						<Route path='/order/:id' component={OrderScreen} />
 						<Route path='/placeorder' component={PlaceOrderScreen} />
 						<Route path='/shipping' component={ShippingScreen} />
@@ -33,7 +39,6 @@ const App = () => {
 						<Route path='/login' component={LoginScreen} />
 						<Route path='/register' component={RegisterScreen} />
 						<Route path='/profile' component={ProfileScreen} />
-						<Route path='/product/:id' component={ProductScreen} />
 						<Route path='/cart/:id?' component={CartScreen} />
 						<Route exact path='/' component={HomeScreen} />
 					</Switch>
