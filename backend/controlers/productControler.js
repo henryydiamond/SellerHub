58632944fd22f592f10b2095d880d6211fa24025
Henry Diamond <henrydiamond94@gmail.com>
2021-09-04 23:13:41 +0400

    Create & Update Product Endpoints

--- backend/controlers/productControler.js
+++ backend/controlers/productControler.js
@@ -35,3 +35,61 @@ export const deleteProductById = asyncHandler(async (req, res) => {
 		throw new Error('Product not found');
 	}
 });
+
+// @desc 	CREATE product
+// @route 	CREATE /api/products
+// @access 	Private/Admin
+export const createProduct = asyncHandler(async (req, res) => {
+	const product = new Product({
+		name: 'Sample Name',
+		price: 0,
+		user: req.user._id,
+		image: '/images/sample.jpg',
+		brand: 'Sample Brand',
+		category: 'Sample Category',
+		countInStock: 0,
+		numReviews: 0,
+		description: 'Sample Description',
+	});
+
+	const createProduct = await product.save();
+	res.status(201).json(createProduct);
+});
+
+// @desc 	Update product
+// @route 	UPDATE /api/products/:id
+// @access 	Private/Admin
+export const updateProduct = asyncHandler(async (req, res) => {
+	const {
+		name,
+		price,
+		image,
+		brand,
+		category,
+		countInStock,
+		numReviews,
+		description,
+	} = req.body;
+
+	const product = await Product.findById(req.params.id);
+
+	if (product && product.user._id.toString() !== req.user._id.toString()) {
+		res.status(401);
+		throw new Error('Unauthorized Action');
+	} else if (product) {
+		product.name = name;
+		product.price = price;
+		product.image = image;
+		product.brand = brand;
+		product.category = category;
+		product.countInStock = countInStock;
+		product.numReviews = numReviews;
+		product.description = description;
+
+		const updatedProduct = await product.save();
+		res.json(updatedProduct);
+	} else {
+		res.status(404);
+		throw new Error('Product not found');
+	}
+});
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,13 +4,16 @@ import {
 	getProducts,
 	getProductById,
 	deleteProductById,
+	createProduct,
+	updateProduct,
 } from '../controlers/productControler.js';
 import { protect, isAdmin } from '../middleware/authMiddleware.js';
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, isAdmin, createProduct);
 router
 	.route('/:id')
 	.get(getProductById)
-	.delete(protect, isAdmin, deleteProductById);
+	.delete(protect, isAdmin, deleteProductById)
+	.put(protect, isAdmin, updateProduct);
 
 export default router;
